be40c85b573cd426bce1aa025f3c1b9b9914e3da
ehye <ehye@users.noreply.github.com>
2026-05-29 01:59:40 +0000

    Validations in blog creation

--- bloglist/app/actions/blog.ts
+++ bloglist/app/actions/blog.ts
@@ -5,7 +5,7 @@ import { revalidatePath } from 'next/cache'
 import { addBlog, addLikes } from '../services/blogs'
 import { auth } from '../auth'
 
-export const createBlog = async (formData: FormData) => {
+export const createBlog = async (prevState: { error: string }, formData: FormData) => {
   const session = await auth()
   if (!session) {
     redirect("/login")
@@ -15,6 +15,17 @@ export const createBlog = async (formData: FormData) => {
   const author = formData.get('author') as string
   const url = formData.get('url') as string
   const likes = parseInt(formData.get('likes') as string, 10) || 0
+
+  if (!title || title.length < 5) {
+    return { error: 'Title must be at least 5 characters long' }
+  }
+  if (!author || author.length < 5) {
+    return { error: 'Author must be at least 5 characters long' }
+  }
+  if (!url || url.length < 5) {
+    return { error: 'Url must be at least 5 characters long' }
+  }
+
   await addBlog(title, author, url, likes)
 
   revalidatePath('/blogs')
--- bloglist/app/blogs/new/page.tsx
+++ bloglist/app/blogs/new/page.tsx
@@ -1,10 +1,15 @@
+'use client'
+
+import { useActionState } from "react"
 import { createBlog } from '@/app/actions/blog'
 
 const NewBlog = () => {
+  const [state, formAction] = useActionState(createBlog, { error: "" })
+
   return (
     <div>
       <h2>Create a new blog</h2>
-      <form action={createBlog}>
+      <form action={formAction}>
         <div>
           <label>
             Title
@@ -30,6 +35,7 @@ const NewBlog = () => {
           </label>
         </div>
         <button type="submit">Create</button>
+        {state.error && <p style={{ color: "red" }}>{state.error}</p>}
       </form>
     </div>
   )
