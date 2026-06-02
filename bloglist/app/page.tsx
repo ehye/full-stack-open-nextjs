85eaa24fa6b836fa3674414f7e517867b64dde12
ehye <ehye@users.noreply.github.com>
2026-06-02 02:39:50 +0000

    Static homepage from markdown

--- bloglist/app/page.tsx
+++ bloglist/app/page.tsx
@@ -1,10 +1,11 @@
+import Homepage from "./home/page.mdx"
+
 const Home = () => {
   return (
-    <div>
-      <div>
-        <h2>blog list</h2>
-      </div>
+    <div className="markdown max-w-6xl mx-auto">
+      <Homepage />
     </div>
   )
 }
+
 export default Home
--- bloglist/next.config.ts
+++ bloglist/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1", "localhost"],
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  extension: /\.(md|mdx)$/,
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
